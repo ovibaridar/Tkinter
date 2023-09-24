87ff06ad2f52d7633e2588956d68347d410804f2
ovibaridar <ovibaridar06@gmail.com>
2023-09-25 05:16:59 +0600

    First GUI using python

--- main.py
+++ main.py
@@ -1,3 +1,10 @@
 from tkinter import *
+
 root = Tk()
+
+root.title("My first GUI")
+root.geometry("500x500")
+root.wm_iconbitmap("img/images.ico")
+# root.resizable(False, False)
+
 root.mainloop()
